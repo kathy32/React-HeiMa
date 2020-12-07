d415a8ca3dd9a6da5de4f42d2187b87b6245d6e6
kathyC <575949391@qq.com>
2020-12-07 15:53:41 +0800

    feat:style行内样式

--- 01.webpack-base/src/components/CmtItem.jsx
+++ 01.webpack-base/src/components/CmtItem.jsx
@@ -1,12 +1,51 @@
 import React from 'react'
 
 
+// 样式封装方式一：将样式对象和 UI 结构分离
+const itemStyle = {
+  border: '1px dashed #ccc',
+  margin: '10px',
+  padding: '10px',
+  boxShadow: '0 0 10px #ccc'
+}
+
+const userStyle = {
+  fontSize: '14px'
+}
+
+const contentStyle = {
+  fontSize: '12px'
+}
+
+
+// 样式封装方式二：合并成一个大的样式对象
+const Styles = {
+  itemStyle: {
+     border: '1px dashed #ccc',
+     margin: '10px',
+     padding: '10px',
+     boxShadow: '0 0 10px #ccc'
+   },
+  userStyle: {
+     fontSize: '14px'
+   },
+  contentStyle: {
+     fontSize: '12px'
+   }
+   
+ }
+
+ 
+// 样式封装方式三：抽离为单独的样式表模块
+import styles from '@/components/styles'
+
+
 // 使用 function 构造函数，定义普通的无状态组件
 // 注意：组件名必须为首字母大写！因为小写会被解析为 html 自带标签，从而报错
 // 全选 props 快捷键: command + d
 export default function CmtItem (props) {
-  return <div>
-    <h2>评论人：{props.user}</h2>
-    <p>评论内容：{props.content}</p>
+  return <div style={styles.itemStyle}>
+    <h2 style={styles.userStyle}>评论人：{props.user}</h2>
+    <p style={styles.contentStyle}>评论内容：{props.content}</p>
   </div>
 }
---
+++ 01.webpack-base/src/components/styles.js
@@ -0,0 +1,17 @@
+export default {
+ itemStyle: {
+    border: '1px dashed #ccc',
+    margin: '10px',
+    padding: '10px',
+    boxShadow: '0 0 10px #ccc'
+  },
+  
+ userStyle: {
+    fontSize: '14px'
+  },
+  
+ contentStyle: {
+    fontSize: '12px'
+  }
+  
+}
