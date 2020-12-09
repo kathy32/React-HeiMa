769287a10fe2b236cc1c86e6cc90a0f60beae418
kathyC <575949391@qq.com>
2020-12-09 20:39:04 +0800

    feat:在 react 中为按钮绑定点击事件

--- 01.webpack-base/src/components/BindEvent.jsx
+++ 01.webpack-base/src/components/BindEvent.jsx
@@ -13,12 +13,12 @@ export default class BindEvent extends React.Component {
       BindEvent 组件事件
       <hr/>
       {/* 在 react 中，有一套自己的事件绑定机制：事件名是小驼峰命名 */}
-      <button onClick={ () => { this.myClickHandler() }}>按钮</button>
+      <button onClick={ () => this.myClickHandler('lokit') }>按钮</button>
     </div>
   }
 
   // 实例方法
-  myClickHandler = () => {
-    console.log('ok')
+  myClickHandler = (arg1) => {
+    console.log('ok - ' + arg1)
   }
 }
