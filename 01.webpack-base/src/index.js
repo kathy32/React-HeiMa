10df0a23738f9ed06bb1a061105c2af148808a93
kathyC <575949391@qq.com>
2020-12-09 20:37:01 +0800

    feat:在 react 中为按钮绑定点击事件

--- 01.webpack-base/src/index.js
+++ 01.webpack-base/src/index.js
@@ -2,11 +2,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import CmtList from '@/components/CmtList2'
+import BindEvent from '@/components/BindEvent'
 
 
 // 3. 调用 render 函数渲染
 ReactDOM.render(<div>
-  {/* <h1>这是评论列表组件</h1> */}
-  <CmtList></CmtList>
+  <BindEvent></BindEvent>
 </div>, document.getElementById('app'))
