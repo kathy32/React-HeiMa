9d4a8a6fc1afae3edab316e5464704e5478c0700
kathyC <575949391@qq.com>
2020-12-09 20:44:57 +0800

    feat:使用 this.setState 修改 state 上的数据

--- 01.webpack-base/src/components/BindEvent.jsx
+++ 01.webpack-base/src/components/BindEvent.jsx
@@ -5,7 +5,9 @@ export default class BindEvent extends React.Component {
   constructor () {
     super()
     // 私有数据
-    this.state = {}
+    this.state = {
+      msg: '哈哈哈'
+    }
   }
 
   render () {
@@ -14,11 +16,17 @@ export default class BindEvent extends React.Component {
       <hr/>
       {/* 在 react 中，有一套自己的事件绑定机制：事件名是小驼峰命名 */}
       <button onClick={ () => this.myClickHandler('lokit') }>按钮</button>
+      <h1>{this.state.msg}</h1>
     </div>
   }
 
   // 实例方法
   myClickHandler = (arg1) => {
     console.log('ok - ' + arg1)
+
+    // 修改状态值推荐使用 this.setState() 而不是 this.state.msg 赋值（单向数据绑定）
+    this.setState({
+      msg: 'hahaha~' + arg1
+    })
   }
 }
